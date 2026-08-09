5ebd9a8c4867210d866e46d6d4de98492d6c5282
Varshaah11 <159683883+Varshaah11@users.noreply.github.com>
2026-08-09 11:12:31 +0530

    fix: export AppLogger from database module

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -5,6 +5,6 @@ import { PrismaService } from '@/database/prisma.service';
 @Global()
 @Module({
   providers: [PrismaService, AppLogger],
-  exports: [PrismaService],
+  exports: [PrismaService, AppLogger],
 })
 export class DatabaseModule {}
